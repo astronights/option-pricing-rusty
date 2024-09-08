43558bfd95cfda89ccb69262c05ff43a953f58bc
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-09-08 21:53:23 +0800

    Black Scholes Model

--- src/models/black_scholes.rs
+++ src/models/black_scholes.rs
@@ -2,11 +2,46 @@ use crate::OptionPricingModel;
 
 pub struct BlackScholesModel {
     pub underlying: f64,
-    pub strike: f64
+    pub strike: f64,
+    pub maturity: f64,
+    pub volatility: f64,
+    pub risk_free_rate: f64
 }
 
 impl OptionPricingModel for BlackScholesModel {
+
+    fn calculate_d1_d2(&self) -> (f64, f64) {
+        let d1 = (f64::ln(self.underlying / self.strike)
+            + (self.risk_free_rate + 0.5 * self.volatility.powi(2)) * self.maturity)
+            / (self.volatility * f64::sqrt(self.maturity))
+
+        let d2 = d1 - self.volatility * f64::sqrt(self.maturity);
+
+        (d1, d2)
+    }
+
+    fn normal_cdf(X: f64) -> f64 {
+        let normal = Normal::new(0.0, 1.0).unwrap();
+        normal.cdf(x)
+    }
+
+    pub fn call_price(&self) -> f64 {
+        let (d1, d2) = self.calculate_d1_d2();
+        let nd1 = Self::normal_cdf(d1);
+        let nd2 = Self::normal_cdf(d2);
+
+        self.underlying * nd1 - self.strike * E.powf(-self.risk_free_rate * self.maturity) * nd2
+    }
+
+    pub fn put_price(&self) -> f64 {
+        let (d1, d2) = self.calculate_d1_d2();
+        let nd1 = Self::normal_cdf(-d1);
+        let nd2 = Self::normal_cdf(-d2);
+
+        self.strike * E.powf(-self.risk_free_rate * self.maturity) * nd2 - self.underlying * nd1
+    }
+
     fn price(&self) -> f64 {
-        1.0 //TODO
+        self.call_price()
     }
 }
