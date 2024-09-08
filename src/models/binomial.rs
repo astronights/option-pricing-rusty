348221eb827d70d505f212904f4996e86896d2dd
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2024-09-08 21:39:24 +0800

    Adding binomial schema

--- src/models/binomial.rs
+++ src/models/binomial.rs
@@ -2,7 +2,11 @@ use crate::OptionPricingModel;
 
 pub struct BinomialModel {
     pub underlying: f64,
-    pub strike: f64
+    pub strike: f64,
+    pub maturity: f64,
+    pub volatility: f64,
+    pub risk_free_rate: f64,
+    pub steps: u32,
 }
 
 impl OptionPricingModel for BinomialModel {
